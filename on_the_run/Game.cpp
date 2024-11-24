4c1240caaae3bd4fdee315c124d083586b6c1b79
MagnusMacphee <123734481+Gustafsonn@users.noreply.github.com>
2024-11-24 16:54:05 +0000

    Added a base game loop and character sprites

---
+++ on_the_run/Game.cpp
@@ -0,0 +1,38 @@
+Game::game()
+:mWindow(sf::VideoMode(1920, 1080), "On The Run")
+, mPlayer()
+{
+	mPlayer.setRadius(40.f);
+	mPlayer.setPosition(100.f, 100.f);
+	mPlayer.setFillColor(sf::Color::Cyan);
+}
+void Game::run()
+{
+	while (mWindow.isOpen())
+	{
+		processEvents();
+		update();
+		render();
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			mWindow.close();
+	}
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
+}
---
+++ on_the_run/Game.h
@@ -0,0 +1,21 @@
+class Game
+{
+	public:
+				Game();
+		void	run();
+			
+	private:
+		void	processEvents();
+		void	update();
+		void	render();
+		
+	private:
+		sf::RenderWindow mWindow;
+		sf::CircleShape mPlayer;
+};
+
+int main()
+{
+	Game game;
+	game.run();
+}
