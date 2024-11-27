4c658fcff6ff562be8554219d84ca5ba9fb79c0c
MagnusMacphee <123734481+Gustafsonn@users.noreply.github.com>
2024-11-27 03:11:39 +0000

    Added new movement
    The player can now turn their car to face the direction they want to go and then go forward in that direction

--- on_the_run/Player.cpp
+++ on_the_run/Player.cpp
@@ -14,7 +14,7 @@ Player::Player()
 	mPlayer.setTexture(mPlayerTexture);
 	mPlayer.setTextureRect(sf::IntRect(0, 0, 30, 30));// Takes a portion of the sprite sheet
 	mPlayer.setScale(sf::Vector2f(10, 10));
-	mPlayer.setOrigin(sf::Vector2f(8, 8));
+	mPlayer.setOrigin(sf::Vector2f(15, 8));
 	mPlayer.setPosition(100.f, 100.f);
 }
 
@@ -39,16 +39,41 @@ void Player::updateMovement(sf::Time deltaTime)
 	sf::Vector2f movement(0.f, 0.f);
 	// Sets movement speed for when a key is pressed
 	movementSpeed = 100.f;
-	// Moves the player in the a direction depending on what/if a key is pressed
-	if (pIsMovingUp)
-		movement.y -= 1.f;
-	if (pIsMovingDown)
-		movement.y += 1.f;           
-	if (pIsMovingLeft)
-		movement.x -= 1.f;
-	if (pIsMovingRight)
-		movement.x += 1.f;
 
-	//calculates movement trajectory
-	mPlayer.move(movement * deltaTime.asSeconds() * movementSpeed);
+	float rotation = mPlayer.getRotation();
+	float angleInRadians = rotation * (3.14159f / 180.f);
+
+	// Moves the player in the a direction depending on what/if a key is pressed
+	if (pIsMovingUp) {
+		movement.x = 0.f;
+		movement.y = -1.f;
+		mPlayer.setOrigin(sf::Vector2f(15, 8));
+	}
+	if (pIsMovingDown) {
+		movement.x = 0.f;
+		movement.y = 1.f;
+		mPlayer.setOrigin(sf::Vector2f(15, 15));
+	}
+	if (pIsMovingLeft) {
+		mPlayer.rotate(-90.f * deltaTime.asSeconds());
+	}
+	if (pIsMovingRight) {
+		mPlayer.rotate(90.f * deltaTime.asSeconds());
+	}
+
+	if (movement.x != 0.f || movement.y != 0.f)
+	{
+		float rotation = mPlayer.getRotation();
+		float angleInRadians = rotation * (3.12159f / 180.f);
+
+		float cosRotation = cos(angleInRadians);
+		float sinRotation = sin(angleInRadians);
+
+		sf::Vector2f rotatedMovement;
+		rotatedMovement.x = movement.x * cosRotation - movement.y * sinRotation;
+		rotatedMovement.y = movement.x * sinRotation + movement.y * cosRotation;
+
+		//calculates movement trajectory
+		mPlayer.move(rotatedMovement * deltaTime.asSeconds() * movementSpeed);
+	}
 }
